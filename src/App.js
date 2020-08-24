c62ae7a0d08f12a6f132dd15ef39e27341337c6e
Tom Meyrick <tomgmeyrick@gmail.com>
2020-08-24 14:25:56 +0100

    Paragraph challenge complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Paragraph from './Paragraph';
 
 const App = () => (
+    <>
     <Header />
+    <Paragraph />
+    </>
   );
 
 export default App;
---
+++ src/Paragraph.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Paragraph = () => (
+    <article className="col-md-6">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</p>
+    </article>
+);
+
+export default Paragraph;
