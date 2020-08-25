2f18db7bc8c213f7f09a738dfd760e0ab1fa2a3c
Tom Meyrick <tomgmeyrick@gmail.com>
2020-08-25 12:48:59 +0100

    Refactored People component

--- src/People.js
+++ src/People.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const People = ({ names }) => (
-    names ? (
-        <ul className="list-group">
-            { names.map((name, i) => (
-                <li className="list-group-item" key={ i }>{ name }</li>
-            )) }
+class People extends Component
+{
+    render() {
+    const{ names } = this.props;
+    return (
+        names ? (
+        <ul onClick={() => console.log("Hello World")} className="list-group">
+        { names.map((name, i) => (
+            <li className="list-group-item" key={ i }>{ name }</li>
+        )) }
         </ul>
-    ) : <p>Nothing to see here</p>
-);
+        ) : <p>Nothing to see here</p>
+    );
+    }
+}
 
 export default People;
