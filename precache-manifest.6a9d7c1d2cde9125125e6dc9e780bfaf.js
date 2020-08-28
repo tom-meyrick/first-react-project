d183d8ed7e163adcf3de3e32e857536b6fe2fdf7
Tom Meyrick <tomgmeyrick@gmail.com>
2020-08-28 12:07:46 +0100

    Updates

---
+++ precache-manifest.6a9d7c1d2cde9125125e6dc9e780bfaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715e5cf21bbd5b7d3aa546c3c71aa8e0",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "3f48907d67c62be13295",
+    "url": "/first-react-project/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "eb30a87c9647032b6126",
+    "url": "/first-react-project/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "3f48907d67c62be13295",
+    "url": "/first-react-project/static/js/2.a9e03196.chunk.js"
+  },
+  {
+    "revision": "c3cb654a3a7d27a576aed8d503b856d8",
+    "url": "/first-react-project/static/js/2.a9e03196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb30a87c9647032b6126",
+    "url": "/first-react-project/static/js/main.ee6f4b83.chunk.js"
+  },
+  {
+    "revision": "935b682e5b1a32ab66c2",
+    "url": "/first-react-project/static/js/runtime-main.5d1a0e17.js"
+  }
+]);
